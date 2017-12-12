412feb56100f82383f293d0f9a9b45808d8ea697
Alexander Korzun <korzun.sas@mail.ru>
2017-12-12 21:24:30 +0300

    Переработан парсинг опций

--- src/args.c
+++ src/args.c
@@ -15,23 +15,7 @@ const int OPTTP_OPT = 2;
 const int OPTTP_SKIP = 3;
 const int OPTTP_UNK = 0;
 
-
-static bool contains_chr(string_t str, char chr)
-{
-    if (str == NULL)
-    {
-        return false;
-    }
-    size_t len = strlen(str);
-
-    for (size_t i = 0; i < len; ++i)
-    {
-        if (str[i] == chr)
-            return true;
-    }
-    return false;
-}
-static int get_long_opt(string_t opt, const char *pattern)
+static int get_opt(string_t opt, const char *pattern)
 {
     if (treat_all_as_files)
     {
@@ -43,26 +27,6 @@ static int get_long_opt(string_t opt, const char *pattern)
         return OPTTP_SKIP;
     }
     if (strcmp(opt, pattern) == 0)
-    {
-        return OPTTP_OPT;
-    }
-    else
-    {
-        return OPTTP_UNK;
-    }
-}
-static int get_short_opt(string_t opt, char pattern)
-{
-    if (treat_all_as_files)
-    {
-        return OPTTP_FILE;
-    }
-    if (strcmp(opt, "--") == 0)
-    {
-        treat_all_as_files = true;
-        return OPTTP_SKIP;
-    }
-    if (strlen(opt) >= 2 && opt[0] == '-' && opt[1] != '-' && contains_chr(opt, pattern))
     {
         return OPTTP_OPT;
     }
@@ -91,7 +55,7 @@ void parse_args(int argc, char **argv)
     {
         bool any_opt = false;
         int result;
-        result = get_long_opt(argv[i], "--help");
+        result = get_opt(argv[i], "--help");
         if (result != OPTTP_UNK)
         {
             any_opt = true;
@@ -112,27 +76,7 @@ void parse_args(int argc, char **argv)
             continue;
         }
 
-        result = get_short_opt(argv[i], 'n');
-        if (result != OPTTP_UNK)
-        {
-            any_opt = true;
-        }
-        if (result == OPTTP_SKIP)
-        {
-            continue;
-        }
-        if (result == OPTTP_FILE)
-        {
-            opts.input_type = IT_FILE;
-            infile = argv[i];
-            //continue;
-        }
-        if (result == OPTTP_OPT)
-        {
-            opts.sort_comparison = SC_NUMERIC;
-            //continue;
-        }
-        result = get_long_opt(argv[i], "--numeric");
+        result = get_opt(argv[i], "-n");
         if (result != OPTTP_UNK)
         {
             any_opt = true;
@@ -152,7 +96,8 @@ void parse_args(int argc, char **argv)
             opts.sort_comparison = SC_NUMERIC;
             continue;
         }
-        result = get_short_opt(argv[i], 'v');
+
+        result = get_opt(argv[i], "--numeric");
         if (result != OPTTP_UNK)
         {
             any_opt = true;
@@ -165,14 +110,34 @@ void parse_args(int argc, char **argv)
         {
             opts.input_type = IT_FILE;
             infile = argv[i];
-            //continue;
+            continue;
+        }
+        if (result == OPTTP_OPT)
+        {
+            opts.sort_comparison = SC_NUMERIC;
+            continue;
+        }
+        result = get_opt(argv[i], "-v");
+        if (result != OPTTP_UNK)
+        {
+            any_opt = true;
+        }
+        if (result == OPTTP_SKIP)
+        {
+            continue;
+        }
+        if (result == OPTTP_FILE)
+        {
+            opts.input_type = IT_FILE;
+            infile = argv[i];
+            continue;
         }
         if (result == OPTTP_OPT)
         {
             opts.output_type = OT_VERSION;
-            //continue;
+            continue;
         }
-        result = get_long_opt(argv[i], "--version");
+        result = get_opt(argv[i], "--version");
         if (result != OPTTP_UNK)
         {
             any_opt = true;
@@ -196,6 +161,10 @@ void parse_args(int argc, char **argv)
         if (!any_opt && argv[i][0] == '-')
         {
             show_usage_and_exit(1);
+        }
+        else if (!any_opt)
+        {
+            opts.input_file = argv[i];
         }
     }
     if (opts.input_type == IT_FILE)
