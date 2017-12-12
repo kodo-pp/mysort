f25e3392cdfe7b1bc61992706bb03b19d4ec4003
Alexander Korzun <korzun.sas@mail.ru>
2017-12-13 00:03:40 +0300

    Оно работает

--- src/sort.c
+++ src/sort.c
@@ -1,95 +1,18 @@
 #include <mysort/sort.h>
 #include <mysort/types.h>
-//#include <mysort/funcs/comp.h>
-//#include <mysort/funcs/sort.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <mysort/die.h>
 #include <mysort/args.h>
 #include <string.h>
-
-/*
-static size_t bufsize = 1024, bufdiff = bufsize;
-static void read_data(char **buf)
-{
-    *buf = malloc(bufsize);
-    if (*buf == NULL)
-    {
-        die("memory allocation error (at read_data)");
-    }
-
-    FILE *in;
-
-    if (opts.input_type == IT_STDIN)
-    {
-        in = fopen("/dev/stdin", "rb");
-        if (in == NULL)
-        {
-            free(*buf);
-            die("unable to open /dev/stdin (at read_data)");
-        }
-    }
-    else
-    {
-        in = fopen(opts.input_file, "rb");
-        if (in == NULL)
-        {
-            free(*buf);
-            die("unable to open input file (at read_data)");
-        }
-    }
-
-    size_t bytes = 0;
-    while (true)
-    {
-        bytes = fread(*buf, 1, bufdiff, in);
-        if (bytes != bufdiff)
-        {
-            bufsize -= bufdiff - bytes;
-            break;
-        }
-        else
-        {
-            bufdiff = bufsize;
-            bufsize *= 2;
-            *buf = realloc(*buf, bufsize);
-            if (*buf == NULL)
-            {
-                fclose(in);
-                die("memory reallocation error (at read_data)");
-            }
-        }
-    }
-    fclose(in);
-    return bufsize;
-}
-
-static size_t get_strs(char *buf, size_t bufsize, char ***strs)
-{
-    if (buf == NULL)
-    {
-        die("buf is NULL (at get_strs)");
-    }
-
-    size_t count = 0;
-    for (size_t i = 0; i < bufsize; ++i)
-    {
-        if (buf[i] == '\n')
-        {
-            ++count;
-        }
-    }
-    *strs = malloc(count * sizeof(char *));
-    int begin = 0;
-    for (size_t i = 0; i < bufsize; ++i)
-    {
-
-    }
-}
-*/
+#include <ctype.h>
 
 compare_t cf_normal(char *first, char *second)
 {
+    if (first == NULL || second == NULL)
+    {
+        die ("argument in NULL (at cf_normal)");
+    }
     compare_t cmp = (compare_t) strcmp(first, second);
     if (cmp < 0)
     {
@@ -105,6 +28,62 @@ compare_t cf_normal(char *first, char *second)
     }
 }
 
+compare_t cf_numeric(char *first, char *second)
+{
+    //printf("Hi\n");
+    if (first == NULL || second == NULL)
+    {
+        die ("argument in NULL (at cf_numeric)");
+    }
+    bool eqlennumflag = false;
+    for (int i = 0; ; ++i)
+    {
+
+        if (isdigit(first[i]) && isdigit(second[i]) && !eqlennumflag)
+        {
+            int j1 = i;
+            int j2 = i;
+            while (first[j1] != '\0' && isdigit(first[j1]))
+            {
+                ++j1;
+            }
+            while (second[j2] != '\0' && isdigit(second[j2]))
+            {
+                ++j2;
+            }
+            if (j1 < j2)
+            {
+                return CMP_LESS;
+            }
+            else if (j1 > j2)
+            {
+                return CMP_MORE;
+            }
+            else
+            {
+                eqlennumflag = true;
+            }
+        }
+        if (first[i] == '\0' && second[i] == '\0')
+        {
+            return CMP_EQ;
+        }
+        else if (first[i] < second[i])
+        {
+            return CMP_LESS;
+        }
+        else if (first[i] > second[i])
+        {
+            return CMP_MORE;
+        }
+        if (!isdigit(first[i]))
+        {
+            eqlennumflag = false;
+        }
+    }
+
+}
+
 void sf_comb(char **data, int count, compfunc_t cmp)
 {
     //printf(">>%d\n", count);
@@ -112,18 +91,35 @@ void sf_comb(char **data, int count, compfunc_t cmp)
     {
         die("data == NULL (at sf_comb)");
     }
-    for (int i = 0; i < count; ++i)
+
+    int step = count - 1;
+    while (step > 0)
     {
-        for (int j = 0; j < count - 1; ++j)
+        for (int j = 0; j + step < count; ++j)
         {
             //printf(">>>%d\n", cmp(data[j], data[j+1]));
-            if (cmp(data[j], data[j+1]) == CMP_MORE)
+            if (cmp(data[j], data[j+step]) == CMP_MORE)
             {
                 //printf("    swap(data[%d{,+1}])\n", j);
                 char *tmp = data[j];
-                data[j] = data[j+1];
-                data[j+1] = tmp;
+                data[j] = data[j+step];
+                data[j+step] = tmp;
             }
+        }
+        step /= 1.2;
+    }
+
+    bool is_sorted = false;
+    while (!is_sorted)
+    {
+        is_sorted = true;
+        for (int j = 0; j < count - 1; ++j)
+        if (cmp(data[j], data[j+1]) == CMP_MORE)
+        {
+            is_sorted = false;
+            char *tmp = data[j];
+            data[j] = data[j+1];
+            data[j+1] = tmp;
         }
     }
 }
@@ -136,8 +132,8 @@ static compfunc_t get_comparator_func()
             return cf_normal;
             break;
         case SC_NUMERIC:
-            //return cf_numeric;
-            return cf_normal;
+            return cf_numeric;
+            //return cf_normal;
             break;
         default:
             return cf_normal;
