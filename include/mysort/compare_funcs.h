55e0eb411e331ea61fa57335ecd13e083308c898
Alexander Korzun <korzun.sas@mail.ru>
2017-12-20 00:40:53 +0300

    Да почему эти файлы не включились в прошлый коммит?

---
+++ include/mysort/compare_funcs.h
@@ -0,0 +1,9 @@
+#ifndef MYSORT_COMPARE_FUNCS_H
+#define MYSORT_COMPARE_FUNCS_H
+
+#include <mysort/types.h>
+
+compare_t cf_normal(char *, char *);
+compare_t cf_numeric(char *, char *);
+
+#endif
---
+++ include/mysort/sort_funcs.h
@@ -0,0 +1,8 @@
+#ifndef MYSORT_SORT_FUNCS_H
+#define MYSORT_SORT_FUNCS_H
+
+#include <mysort/types.h>
+
+void sf_comb(char **, int, compfunc_t);
+
+#endif
