c8968c0c8e5fa03a2976d1d46a8a853b8b902afd
Alexander Korzun <korzun.sas@mail.ru>
2018-03-13 20:30:06 +0300

    Изменил скрипт тестирования и формат тестов

--- test.sh
+++ test.sh
@@ -2,40 +2,58 @@
 
 if [ -z $1 ]; then
     echo "Usage: $0 <mysort_binary_name>"
+    echo "mysort binary should be located in the top-level directory of the project"
+    echo "  (in which the subdirectory tests/ is located)"
     echo "Please consider using \`make check' instead of this script"
     exit 1
 fi
 
-progname="$1"
+export execname="../../$1"
 
-flags=(
-dummy
-""
-"-n"
-""
-)
+function die() {
+    echo "Error: $@" >&2
+    exit 1
 
-num_tests=3
+    # If bash still continues to run
+    kill $$
+}
 
-failed_tests=0
+function warn() {
+    echo "Warning: $@" >&2
+}
 
-for i in `seq 1 $num_tests`; do
-    echo -n "Test $i: "
-    cat tests/test"$i".txt | ./"$progname" ${flags[$i]} > tests/test"$i".out || { echo -e "\e[1;41;37mFatal error while running program\e[0m"; exit 120; }
-    if diff tests/test"$i".out tests/test"$i".gt >/dev/null; then
-        echo -e "\e[1;32mPass\e[0m"
-    else
-        echo -e "\e[1;31mFail\e[0m"
-        failed_tests=$[$failed_tests + 1]
+{
+
+tests_count=0
+passed_tests_count=0
+
+cd tests || die "tests/ directory not found"
+for i in test*; do
+    cd "$i" || { warn "unable to access directory '$i'"; continue; }
+
+    options="`cat options.txt 2>/dev/null`"
+    input="input.txt"
+    real_output="output.txt"
+    real_stderr="stderr.txt"
+    ground_truth_output="gt-output.txt"
+
+    passed="no"
+
+    cat "$input" | "$execname" $options >"$real_output" 2>"$real_stderr"
+
+    if diff "$real_output" "$ground_truth_output" &>/dev/null; then
+        passed="yes"
+        (( ++passed_tests_count ))
     fi
+    (( ++tests_count ))
+    echo -e "Test: $i Passed: $passed"
+
+    cd ..
 done
 
-echo "***************"
-echo "*** SUMMARY ***"
-echo "***************"
+# FIXME: Не работает
+#echo "=== Summary ==="
+#echo "$tests_count tests total, $passed_tests_count passed, $(($tests_count-$passed_tests_count)) failed"
 
-echo "Total tests: $num_tests"
-echo "Failed tests: $failed_tests"
-echo "Passed tests: $[$num_tests - $failed_tests]"
-
-exit $failed_tests
+} | column -t
+exit 0
