4c0966a8370bf72d7a5001f2203d9b4e23d9ba2f
Alexander Korzun <korzun.sas@mail.ru>
2018-03-21 11:26:24 +0300

    Add locale support in test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,50 @@
 #!/bin/bash
 
+declare -Ag lang
+
+if locale | grep LC_MESSAGES | grep -q ru_RU; then
+    lang[Usage]='Usage'
+    lang[usage_info]='<исполняемый_файл_mysort>
+Исполняемый файл mysort должен быть расположен в папке проекта
+  (в которой расположен подкатолог tests/)
+Советуем использовать "make check" вместо непосредственного выполнения этого скрипта'
+    lang[Error]='Ошибка'
+    lang[Warning]='Предепреждение'
+    lang[Usage]='Использование'
+    lang[tests_not_found]='папка tests/ не найдена'
+    lang[unable_to_access_dir]='невозможно получить доступ к папке'
+    lang[Test]='Тест'
+    lang[Passed]='Пройден'
+    lang[yes]='да'
+    lang[no]='нет'
+else
+    lang[Usage]='Usage'
+    lang[usage_info]='<mysort_binary_name>
+mysort binary should be located in the top-level directory of the project
+  (in which the subdirectory tests/ is located)
+Please consider using "make check" instead of directly running this script'
+    lang[Error]='Error'
+    lang[Warning]='Warning'
+    lang[Usage]='Usage'
+    lang[tests_not_found]='tests/ directory not found'
+    lang[unable_to_access_dir]='unable to access directory'
+    lang[Test]='Test'
+    lang[Passed]='Passed'
+    lang[yes]='yes'
+    lang[no]='no'
+fi
+
+
+
 if [ -z $1 ]; then
-    echo "Usage: $0 <mysort_binary_name>"
-    echo "mysort binary should be located in the top-level directory of the project"
-    echo "  (in which the subdirectory tests/ is located)"
-    echo "Please consider using \`make check' instead of this script"
+    echo "${lang[Usage]}: $0 ${lang[usage_info]}"
     exit 1
 fi
 
 export execname="../../$1"
 
 function die() {
-    echo "Error: $@" >&2
+    echo "${lang[Error]}: $@" >&2
     exit 1
 
     # If bash still continues to run
@@ -19,7 +52,7 @@ function die() {
 }
 
 function warn() {
-    echo "Warning: $@" >&2
+    echo "${lang[Warning]}: $@" >&2
 }
 
 {
@@ -27,9 +60,9 @@ function warn() {
 tests_count=0
 passed_tests_count=0
 
-cd tests || die "tests/ directory not found"
+cd tests || die "${lang[tests_not_found]}"
 for i in test*; do
-    cd "$i" || { warn "unable to access directory '$i'"; continue; }
+    cd "$i" || { warn "${lang[unable_to_access_dir]} '$i'"; continue; }
 
     options="`cat options.txt 2>/dev/null`"
     input="input.txt"
@@ -46,7 +79,7 @@ for i in test*; do
         (( ++passed_tests_count ))
     fi
     (( ++tests_count ))
-    echo -e "Test: $i Passed: $passed"
+    echo -e "${lang[Test]}: $i ${lang[Passed]}: ${lang[$passed]}"
 
     cd ..
 done
